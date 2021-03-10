f5a7e16961a7aee88c5bbfb9c609bf558c193ffe
wutongyuqq <314421674@qq.com>
2021-03-10 08:55:03 +0800

    橙子编程我的页面完成提交

--- lib/pages/mine.dart
+++ lib/pages/mine.dart
@@ -41,64 +41,161 @@ class _MinePageState extends State<MinePage> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      appBar: AppBar(
-        title: Text('我的'),
-      ),
-      body:
-      SingleChildScrollView(
-        child:  dataMap['userInfo']==null?Container():Container(
-          alignment: Alignment.center,
-          width: 700.sp,
-          //自定义列表,注意这里传递了参数在类中
-          //child: MyListView(items: dataMap==null?new List():dataMap['lessonList']),
-          child: Column(
+        backgroundColor: Theme
+            .of(context)
+            .backgroundColor,
+        appBar: AppBar(
+          title: Text('我的'),
+        ),
+        body:
+        new ListView(
             children: <Widget>[
-              Stack(
+              new Column(
                 children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      for (int i = 0; i < dataMap['userInfo'].length; i++)
-                        lessonItem(
-                            dataMap['userInfo'][i]
-                        ),
-                    ],
-                  )
+                  Image(image: NetworkImage(
+                      "http://mn.yxg12.cn/images/2/2020/12/TjohDY67LTH1bnBBhOnNTEyYY197T5.jpg"),
+                      width: 600.0,
+                      height: 240.0,
+                      fit: BoxFit.cover),
+                  new Container(
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildButtonColumn("0", '会员积分'),
+                        buildButtonColumn("0.00", '会员余额'),
+                        buildButtonColumn("0", '优惠券'),
+                      ],
+                    ),
+                  ),
+                  mineItem(),
+                  new Container(
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildIconColumn('assets/images/self-icon-nopay.png', '待付款'),
+                        buildIconColumn('assets/images/self-icon-pay.png', '已付款'),
+                        buildIconColumn('assets/images/self-icon-noverify.png', '可核销'),
+                        buildIconColumn('assets/images/self-icon-evaluate.png', '待评价'),
+                      ],
+                    ),
+                  ),
+                  mineListItem("assets/images/self-icon-subscribe.png","订阅消息"),
+                  mineListItem("assets/images/self-icon-vip-service.png","VIP服务"),
+                  mineListItem("assets/images/self-icon-teacher-buy.png","讲师服务"),
+                  mineListItem("assets/images/self-icon-lesson-invite.png","课程邀请"),
+                  mineListItem("assets/images/self-icon-history.png","我的足迹"),
+                  mineListItem("assets/images/self-icon-collect-lesson.png","收藏课程"),
+                  mineListItem("assets/images/self-icon-collect-teacher.png","收藏讲师"),
+                  mineListItem("assets/images/self-icon-collect-teachercenter.png","讲师中心"),
+                  mineListItem("assets/images/self-icon-commission.png","分销中心"),
+                  mineListItem("assets/images/self-icon-study-duration.png","学习时长"),
+                  mineListItem("assets/images/self-icon-coupon.png","优惠券"),
+                  mineListItem("assets/images/self-icon-modify-mobile.png","修改手机"),
                 ],
-              ),
-            ],
+
+              )
+            ]
+        )
+    );
+
+
+  }
+
+
+  Column buildButtonColumn(String labelNum, String label) {
+
+    return new Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        new Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: new Text(
+            labelNum,
+            style: new TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+              color: Color(int.parse('0xff005956')),
+            ),
           ),
         ),
-      ),
+        new Container(
+
+          child: new Text(
+            label,
+            style: new TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+              color: Color(int.parse('0xff005956')),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
 
 
-  Widget lessonItem(lessonMap){
-    return Container(
-      width: 700.sp,
-      height: 200.sp,
-      child: Row(
-          children: [
 
-            new Container(
-              margin: EdgeInsets.fromLTRB(0.sp, 15.sp, 10.sp, 0.sp),//外边距，父容器本身相对外部容器的移动
-              child:  Image(image: NetworkImage((lessonMap==null||lessonMap['images']==null)?'':(Global.baseImageUrl +lessonMap['images'])),width:260.sp,height:200.sp,fit:BoxFit.fill),
+  Column buildIconColumn(String iconPath, String label) {
+    Color color = Theme.of(context).primaryColor;
+    return new Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(iconPath),
+        new Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: new Text(
+            label,
+            style: new TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+              color: color,
             ),
-            new Container(
-                alignment: Alignment.topLeft,
-                width: 420.sp,
-                child:Column(
-                  children: [
-                    Text(lessonMap['nickname'],overflow:TextOverflow.clip,textAlign:TextAlign.left),
-                    Text(lessonMap['openid']==null?"":lessonMap['openid'],overflow:TextOverflow.clip,textAlign:TextAlign.left)
-                  ],
-                )
-            )
-          ]
+          ),
+        ),
+      ],
+    );
+  }
 
-      ),
+  Widget mineItem(){
+    return Row(
+
+        children: [
+          new Container(
+
+            child: new Text(
+              '全部订单',
+              style: new TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          new Text('查看全部订单'),
+          new Icon(
+            Icons.navigate_next_outlined,
+            color: Colors.grey,
+          ),
+
+        ]
+    );
+  }
+
+
+
+
+  Widget mineListItem(String iconPath,String words){
+    return Row(
+        children: [
+          Image.asset(iconPath),
+          new Text(words),
+          new Icon(
+            Icons.navigate_next_outlined,
+            color: Colors.grey,
+          ),
+
+        ]
     );
   }
 
